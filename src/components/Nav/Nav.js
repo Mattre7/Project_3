41b23c373db9c37f0a64743d24fef63e47f432f1
matteichler <matteichler7@gmail.com>
2018-10-16 21:07:16 -0700

    minor files added

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Nav = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a className="navbar-brand" href="/">
+        Log In
+    </a>
+    <a className="navbar-brand" href="/">
+        Create an Account
+    </a>
+  </nav>
+);
+
+export default Nav;
